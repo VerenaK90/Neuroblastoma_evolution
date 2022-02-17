ad5c9f24db6bed2b5da953c7ae0e588bf19b9145
VerenaK90 <47210805+VerenaK90@users.noreply.github.com>
2022-02-17 16:47:28 +0100

    Add files via upload

--- Analysis_and_plots/Survival_analysis.R
+++ Analysis_and_plots/Survival_analysis.R
@@ -8,7 +8,7 @@ if("ECA" %in% colnames(sample.information.80x)){
 survival.information.80x <- sample.information.80x[sample.information.80x$Location %in% c("Primary", "Metastasis"),
                           c("OS.time.(days)", "OS", "EFS", "EFS.time(days)", "ECA.exists", "Ploidy", "Telomere.maintenance.mechanism",
                             "17q.gain", "17.gain", "7.gain", "7q.gain", "1.gain", "1q.gain", "1p.deletion", "11q.deletion", "Sex", 
-                            "Stage", "Age")]
+                            "Stage", "Age", "NB2004", "MYCN.(current.disease.episode)")]
 
 load(paste0(rdata.directory, "MRCA_timing.RData"))
 mutation.time.eca[names(earliest.mutation.time)] <- earliest.mutation.time
@@ -18,7 +18,7 @@ mutation.time.eca.upper[names(earliest.mutation.time)] <- earliest.mutation.time
 rownames(survival.information.80x) <- rownames(sample.information.80x)[sample.information.80x$Location %in% c("Primary", "Metastasis")]
 colnames(survival.information.80x) <- c("OS.time.", "OS", "EFS", "EFS.time", "ECA.exists", "Ploidy", "Telomere.maintenance.mechanism",
                                     "17q.gain", "17.gain", "7.gain", "7q.gain", "1.gain", "1q.gain", "1p.deletion", "11q.deletion", "Sex", 
-                                    "Stage", "Age")
+                                    "Stage", "Age", "NB2004", "MYCN.(current.disease.episode)")
 survival.information.80x$Ploidy[survival.information.80x$Ploidy %in% c(2,4)] <- "2,4"
 
 survival.information.80x$MRCA.time <- mutation.time.mrca[rownames(survival.information.80x)]
@@ -123,10 +123,10 @@ colnames(survival.information.30x)[colnames(survival.information.30x)=="Age.(day
 
 joined.survival.information <- rbind(survival.information.80x[, c("OS.time.", "OS", "EFS", "EFS.time", "ECA.exists", "Ploidy", "Telomere.maintenance.mechanism",
                                                               "17q.gain", "17.gain", "7.gain", "7q.gain", "1.gain", "1q.gain", "1p.deletion", "11q.deletion", "Sex", 
-                                                              "Stage", "Age", "MRCA.time")], 
+                                                              "Stage", "Age", "MRCA.time", "NB2004", "MYCN.(current.disease.episode)")], 
                                      survival.information.30x[, c("OS.time.", "OS", "EFS", "EFS.time", "ECA.exists", "Ploidy", "Telomere.maintenance.mechanism",
                                                                          "17q.gain", "17.gain", "7.gain", "7q.gain", "1.gain", "1q.gain", "1p.deletion", "11q.deletion", "Sex", 
-                                                                         "Stage", "Age", "MRCA.time")])
+                                                                         "Stage", "Age", "MRCA.time", "NB2004", "MYCN.(current.disease.episode)")])
 
 
 
@@ -180,8 +180,66 @@ for(i in 1:nrow(joined.p.value.table)){
 # Fit a Cox proportional hazards model
 
 joined.categorized.by.MRCA$ECA.exists <- as.character(as.logical(joined.categorized.by.MRCA$ECA.exists))
-surv_object <- Surv(time=joined.categorized.by.MRCA$`OS.time.`, event=joined.categorized.by.MRCA$OS) 
+joined.categorized.by.MRCA$NB2004 <- factor(joined.categorized.by.MRCA$NB2004, levels=c("observation", "MRG", "HR"))
+joined.categorized.by.MRCA$TMM.binary <- factor(joined.categorized.by.MRCA$TMM.binary, levels=c("no TMM", "TMM"))
 
-fit.coxph <- coxph(surv_object ~ MRCA.time + TMM.binary + Stage.binary , 
-                   data = joined.categorized.by.MRCA)
 
+## I. univariate Cox regression
+covariates <- c("MRCA.time", "TMM.binary", "NB2004.binary")
+
+univ_formulas <- sapply(covariates,
+                        function(x) as.formula(paste('Surv(joined.categorized.by.MRCA$`OS.time.`, joined.categorized.by.MRCA$OS)~', x)))
+
+univ_models <- lapply( univ_formulas, function(x){coxph(x, data = joined.categorized.by.MRCA)})
+
+# Extract result 
+univ_results <- lapply(univ_models,
+                       function(x){ 
+                         x <- summary(x)
+                         p.value<-signif(x$wald["pvalue"], digits=2)
+                         wald.test<-signif(x$wald["test"], digits=2)
+                         beta<-signif(x$coef[1], digits=2);#coefficient beta
+                         HR <-signif(x$coef[2], digits=2);#exp(beta)
+                         HR.confint.lower <- signif(x$conf.int[,"lower .95"], 2)
+                         HR.confint.upper <- signif(x$conf.int[,"upper .95"],2)
+                         HR <- paste0(HR, " (", 
+                                      HR.confint.lower, "-", HR.confint.upper, ")")
+                         res<-c(beta, HR, wald.test, p.value)
+                         names(res)<-c("beta", "HR (95% CI for HR)", "wald.test", 
+                                       "p.value")
+                         return(res)
+                       })
+res <- t(as.data.frame(univ_results, check.names = FALSE))
+as.data.frame(res)
+
+
+## II. Multivariate analysis
+## check assumptions: 
+## - proportional hazards: ratio of hazard should be constant over time between groups
+
+lapply(univ_models, cox.zph)
+## non significant for any of the covariates; thus the assumption holds.
+for(i in 1:length(univ_models)){
+  test.ph <- cox.zph(univ_models[[i]])
+  print(ggcoxzph(test.ph))
+}
+
+## - linear relationship between covariates and log hazard h(t)
+
+## not an issue for categorial variables and we only have categorial variables
+
+## - influential observations (outliers)
+
+## plots the estimated changes in the regression coefficient after deleting each observation
+
+ggcoxdiagnostics(univ_models[[1]], type = "dfbeta", linear.predictions = F)
+ggcoxdiagnostics(univ_models[[2]], type = "dfbeta", linear.predictions = F)
+ggcoxdiagnostics(univ_models[[3]], type = "dfbeta", linear.predictions = F)
+
+
+## Event-free survival
+surv_object <- Surv(time=joined.categorized.by.MRCA$`EFS.time`, event=joined.categorized.by.MRCA$EFS) 
+
+
+fit.coxph.efs <- coxph(surv_object ~ MRCA.time + TMM.binary + NB2004 , 
+                       data = joined.categorized.by.MRCA)
