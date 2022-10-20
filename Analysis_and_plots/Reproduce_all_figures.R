51a681473e64853ca6afe3d1574db35f076a7fd3
VerenaK90 <47210805+VerenaK90@users.noreply.github.com>
2022-10-20 17:53:27 +0200

    Update Reproduce_all_figures.R

--- Analysis_and_plots/Reproduce_all_figures.R
+++ Analysis_and_plots/Reproduce_all_figures.R
@@ -3,5 +3,4 @@ source(paste0(custom.script.directory, "./Figure_2.R"))
 source(paste0(custom.script.directory, "./Figure_3.R"))
 source(paste0(custom.script.directory, "./Figure_4.R"))
 source(paste0(custom.script.directory, "./Figure_5.R"))
-source(paste0(custom.script.directory, "./Figure_6.R"))
-
+source(paste0(custom.script.directory, "./Figure_7.R"))
